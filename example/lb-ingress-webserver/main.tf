fce36d6ea0a482b48f9ee27dab5702c9aaefa2a8
Ramones C3po <ramonesc3po@gmail.com>
2019-08-05 17:10:47 -0300

    Add new resources

--- example/lb-ingress-webserver/main.tf
+++ example/lb-ingress-webserver/main.tf
@@ -7,6 +7,23 @@ provider "aws" {
 module "ingress-webserver" {
   source = "../../"
 
-  lb_name            = "simple-load-balancer-production"
-  create_lb_listener = true
+  ingress_name           = "webapp"
+  tier                   = "production"
+  lb_name                = "simple-load-balancer-production"
+  ingress_port           = 80
+  create_lb_listener     = true
+
+  rule_condition = [
+    {
+      field  = "path-pattern"
+      values = ["/api"]
+    }
+  ]
+
+  target_group = {
+    port                 = 3000
+    protocol             = "HTTP"
+    deregistration_delay = 60
+    target_type          = "ip"
+  }
 }
