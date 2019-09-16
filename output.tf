6354ca8e4b5128219104236057659ee5dfd6a788
Ramones C3po <ramonesc3po@gmail.com>
2019-09-16 11:22:56 -0300

    Add second output

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "target_group_name" {
   value = element(concat(aws_lb_target_group.this.*.name, list("")), 0)
 }
+
+output "target_group_arn" {
+  value = element(concat(aws_lb_target_group.this.*.arn, list("")), 0)
+}
